9686132bc6bc3cb9b22a3a68a192efe106be6714
SarvajeetPatel <sarvajeet@zealousweb.com>
2024-05-30 12:30:28 +0530

    final project

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
+
+const port = 5000;
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+}));
+
+app.get("/game", (req, res) => {
+    res.status(200).send("Tic Tac Toe Game Server");
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("makeMove", (data) => {
+        io.emit("moveMade", data);
+    });
+
+    socket.on("resetGame", (newGame) => {
+        io.emit("gameReset", newGame);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
